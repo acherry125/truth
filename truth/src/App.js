71c423ee85386dd9f20ae747a9eaca7ebf695b42
Alex Cherry <acherry125@gmail.com>
2017-12-13 22:10:35 +0000

    remember which platform options were searched for

--- truth/src/App.js
+++ truth/src/App.js
@@ -8,13 +8,27 @@ import UserView from 'components/UserView.js'
 
 /*** Top-Level Application ***/
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            version: "2",
+            system: 'xb'
+        }
+        this.setPlatform = this.setPlatform.bind(this);
+    }
+    setPlatform(system, version) {
+        this.setState({
+            version: version,
+            system: system
+        })
+    }
     render() {
         return (
             <BrowserRouter>
                 <div className="App">
                     <Switch>
                         <Route path="/:version/user/:consoleName/:username" render={props => <UserView {...props} />} />
-                        <Route path="/" render={props => <SearchUsers {...props} />} />
+                        <Route path="/" render={props => <SearchUsers version={this.state.version} system={this.state.system} setPlatform={this.setPlatform} {...props} />} />
                     </Switch>
                 </div>
             </BrowserRouter>
--- truth/src/components/SearchUsers.js
+++ truth/src/components/SearchUsers.js
@@ -12,6 +12,7 @@ class SearchUsers extends Component {
         const username = event.target.username.value;
         const consoleType = event.target["console-type"].value;
         const destinyVersion = event.target["destiny-version"].value;
+        this.props.setPlatform(consoleType, destinyVersion);
         this.props.history.push(`/v${destinyVersion}/user/${consoleType}/${username}`);
     }
     render() {
@@ -23,19 +24,19 @@ class SearchUsers extends Component {
                 </div>
                 <form onSubmit={this.checkUsername}>
                     <h2>What is your name Guardian?</h2>
-                    <input type="text" name="username" placeholder="Destiny Username"  />
+                    <input type="text" name="username" placeholder="Destiny Username"/>
                     <div className="radio-buttons">
-                        <input type="radio" name="console-type" value="xb" id="xbox-radio" defaultChecked="true"/>
+                        <input type="radio" name="console-type" value="xb" id="xbox-radio" defaultChecked={this.props.system === "xb"}/>
                         <label htmlFor="xbox-radio">Xbox</label>
-                        <input type="radio" name="console-type" value="ps" id="playstation-radio"/>
+                        <input type="radio" name="console-type" value="ps" id="playstation-radio" defaultChecked={this.props.system === "ps"}/>
                         <label htmlFor="playstation-radio">Playstation</label>
-                        <input type="radio" name="console-type" value="pc" id="pc-radio"/>
+                        <input type="radio" name="console-type" value="pc" id="pc-radio" defaultChecked={this.props.system === "pc"}/>
                         <label htmlFor="pc-radio">PC</label>
                     </div>
                     <div className="radio-buttons">
-                        <input type="radio" name="destiny-version" value="1" id="dest-1"/>
+                        <input type="radio" name="destiny-version" value="1" id="dest-1" defaultChecked={this.props.version === "1"}/>
                         <label htmlFor="dest-1">Destiny 1</label>
-                        <input type="radio" name="destiny-version" value="2" id="dest-2" defaultChecked="true"/>
+                        <input type="radio" name="destiny-version" value="2" id="dest-2" defaultChecked={this.props.version === "2"} />
                         <label htmlFor="dest-2">Destiny 2</label>
                     </div>
                     <input className="submit-button" type="submit" />
